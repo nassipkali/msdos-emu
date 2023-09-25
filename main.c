5fcaa988b3a17800ac25586810b889ceeeda670c
uralskdev <uralskdev@gmail.com>
2023-09-25 23:27:00 +0500

    fixed program without save

--- main.c
+++ main.c
@@ -2,195 +2,195 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DISK_SIZE 20 * 1024 * 1024 // Размер диска (20 МБ)
-#define FILE_SYSTEM_SIGNATURE 0x12345678 // Сигнатура для проверки формата FAT32
+// Максимальное количество поддиректорий
+#define MAX_SUBDIRECTORIES 100
 
-typedef struct {
+// Структура для представления каталогов и файлов
+typedef struct Entry {
     char name[255];
     int is_directory;
+    char content[1024];
+    struct Entry *sub_entries[MAX_SUBDIRECTORIES];
+    int num_sub_entries;
+    struct Entry *parent;
 } Entry;
 
-typedef struct {
-    int signature;
-    Entry* entries;
-    int entry_count;
-    char current_path[255];
-} FileSystem;
-
-// Инициализация файловой системы
-void initialize(FileSystem* fs) {
-    fs->signature = FILE_SYSTEM_SIGNATURE;
-    fs->entries = NULL;
-    fs->entry_count = 0;
-    strcpy(fs->current_path, "/");
+// Функция для создания новой записи (файла или папки)
+Entry* create_entry(char name[], int is_directory, char content[], Entry *parent) {
+    Entry *entry = (Entry*)malloc(sizeof(Entry));
+    strcpy(entry->name, name);
+    entry->is_directory = is_directory;
+    strcpy(entry->content, content);
+    entry->num_sub_entries = 0;
+    entry->parent = parent;
+    return entry;
 }
 
-// Проверка формата диска
-int checkDiskFormat(FileSystem* fs) {
-    return fs->signature == FILE_SYSTEM_SIGNATURE;
-}
-
-// Функция форматирования диска
-void formatDisk(FileSystem* fs) {
-    if (fs->entries != NULL) {
-        free(fs->entries);
+// Функция для добавления подзаписи (файла или папки) к записи
+void add_sub_entry(Entry *parent, Entry *sub_entry) {
+    if (parent->num_sub_entries < MAX_SUBDIRECTORIES) {
+        parent->sub_entries[parent->num_sub_entries] = sub_entry;
+        parent->num_sub_entries++;
     }
-    initialize(fs);
 }
 
-// Функция вывода содержимого текущей директории
-void listDirectory(FileSystem* fs) {
-    if (!checkDiskFormat(fs)) {
-        printf("Unknown disk format\n");
-        return;
+// Функция для поиска записи (файла или папки) по имени внутри папки
+Entry* find_entry_by_name(Entry *parent, const char *name) {
+    for (int i = 0; i < parent->num_sub_entries; i++) {
+        if (strcmp(parent->sub_entries[i]->name, name) == 0) {
+            return parent->sub_entries[i];
+        }
+    }
+    return NULL;
+}
+
+// Функция для вывода содержимого текущей директории
+void list_directory(Entry *current_directory) {
+    if (current_directory->parent != NULL) {
+        printf(".\n");
+        printf("..\n");
+    }
+    else {
+        printf(".\n");
+    }
+    for (int i = 0; i < current_directory->num_sub_entries; i++) {
+        printf("%s\n", current_directory->sub_entries[i]->name);
+    }
+}
+
+// Function to change the current directory
+Entry* change_directory(Entry *current_directory, Entry *root, const char *target) {
+    if (strcmp(target, "..") == 0 && current_directory->parent != NULL) {
+        // Move up one level
+        return current_directory->parent;
+    } else if (strcmp(target, ".") == 0 || strcmp(target, "") == 0) {
+        // Stay in the current directory
+        return current_directory;
+    } else if (strcmp(target, "/") == 0) {
+        // Change to the root directory
+        return root;
     }
 
-    printf(".\n");
-    printf("..\n");
-    for (int i = 0; i < fs->entry_count; i++) {
-        if (strncmp(fs->entries[i].name, fs->current_path, strlen(fs->current_path)) == 0) {
-            char* relative_name = fs->entries[i].name + strlen(fs->current_path);
-            if (relative_name[0] == '/') {
-                relative_name++;
+    if (target[0] == '.' && target[1] == '/') {
+        // Handle paths starting with './' (current directory)
+        target += 2; // Skip the "./" prefix
+    } else if (target[0] == '/') {
+        // Handle absolute paths
+        current_directory = root;
+        target++; // Skip the leading '/'
+    }
+
+    char *path_copy = strdup(target);
+    char *token = strtok(path_copy, "/");
+    Entry *new_directory = current_directory;
+
+    while (token != NULL) {
+        if (strcmp(token, "..") == 0) {
+            if (new_directory->parent != NULL) {
+                new_directory = new_directory->parent;
             }
-            printf("%s\n", relative_name);
+        } else {
+            Entry *found_entry = find_entry_by_name(new_directory, token);
+
+            if (found_entry != NULL && found_entry->is_directory) {
+                new_directory = found_entry;
+            } else {
+                printf("Folder not found: %s\n", token);
+                free(path_copy);
+                return current_directory;
+            }
         }
+        token = strtok(NULL, "/");
+    }
+
+    free(path_copy);
+    return new_directory;
+}
+
+// Функция для создания новой папки
+void create_directory(Entry *current_directory, const char *name) {
+    Entry *new_directory = create_entry(name, 1, "", current_directory);
+    add_sub_entry(current_directory, new_directory);
+}
+
+// Функция для создания нового файла
+void create_file(Entry *current_directory, const char *name, const char *content) {
+    Entry *new_file = create_entry(name, 0, content, current_directory);
+    add_sub_entry(current_directory, new_file);
+}
+
+// Функция для получения полного пути текущей директории
+void get_current_path(Entry *current_directory, char *path) {
+    Entry *stack[MAX_SUBDIRECTORIES]; // Стек для хранения директорий
+    int depth = 0; // Глубина стека
+
+    // Помещаем директории в стек, начиная с текущей и двигаясь вверх по иерархии
+    while (current_directory != NULL) {
+        stack[depth] = current_directory;
+        current_directory = current_directory->parent;
+        depth++;
+    }
+
+    if (depth == 1) {
+        strcat(path, "/");
+        return;
+    }
+
+    depth--; // skip root dir
+
+    // Собираем путь, начиная с корневой директории и двигаясь вниз по стеку
+    while (depth > 0) {
+        depth--;
+        strcat(path, "/");
+        strcat(path, stack[depth]->name);
     }
 }
 
-// Функция для создания каталога
-void createDirectory(FileSystem* fs, const char* name) {
-    if (!checkDiskFormat(fs)) {
-        printf("Unknown disk format\n");
-        return;
-    }
+int main() {
+    Entry *root = create_entry("", 1, "", NULL);
+    Entry *current_directory = root;
 
-    // Проверка, что имя не пустое
-    if (strlen(name) == 0) {
-        printf("Invalid directory name\n");
-        return;
-    }
-
-    // Проверка, что каталог с таким именем не существует
-    for (int i = 0; i < fs->entry_count; i++) {
-        if (fs->entries[i].is_directory && strcmp(fs->entries[i].name, name) == 0) {
-            printf("Directory '%s' already exists\n", name);
-            return;
-        }
-    }
-
-    // Создание каталога
-    Entry* new_entry = malloc(sizeof(Entry));
-    strcpy(new_entry->name, name);
-    new_entry->is_directory = 1;
-    fs->entries = realloc(fs->entries, (fs->entry_count + 1) * sizeof(Entry));
-    fs->entries[fs->entry_count] = *new_entry;
-    fs->entry_count++;
-}
-
-// Функция для создания файла
-void createFile(FileSystem* fs, const char* name) {
-    if (!checkDiskFormat(fs)) {
-        printf("Unknown disk format\n");
-        return;
-    }
-
-    // Проверка, что имя не пустое
-    if (strlen(name) == 0) {
-        printf("Invalid file name\n");
-        return;
-    }
-
-    // Проверка, что файл с таким именем не существует
-    for (int i = 0; i < fs->entry_count; i++) {
-        if (!fs->entries[i].is_directory && strcmp(fs->entries[i].name, name) == 0) {
-            printf("File '%s' already exists\n", name);
-            return;
-        }
-    }
-
-    // Создание файла
-    Entry* new_entry = malloc(sizeof(Entry));
-    strcpy(new_entry->name, name);
-    new_entry->is_directory = 0;
-    fs->entries = realloc(fs->entries, (fs->entry_count + 1) * sizeof(Entry));
-    fs->entries[fs->entry_count] = *new_entry;
-    fs->entry_count++;
-}
-
-// Функция для перехода в другую директорию
-void changeDirectory(FileSystem* fs, const char* path) {
-    if (!checkDiskFormat(fs)) {
-        printf("Unknown disk format\n");
-        return;
-    }
-
-    // Проверка, что путь существует
-    for (int i = 0; i < fs->entry_count; i++) {
-        if (fs->entries[i].is_directory && strcmp(fs->entries[i].name, path) == 0) {
-            strcpy(fs->current_path, path);
-            return;
-        }
-    }
-
-    printf("Path '%s' does not exist\n", path);
-}
-
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <disk_file_path>\n", argv[0]);
-        return 1;
-    }
-
-    // Создание файла диска или открытие существующего
-    FILE* disk_file = fopen(argv[1], "r+b");
-    if (disk_file == NULL) {
-        disk_file = fopen(argv[1], "w+b");
-        if (disk_file == NULL) {
-            perror("Error opening disk file");
-            return 1;
-        }
-        // Устанавливаем размер файла диска
-        fseek(disk_file, DISK_SIZE - 1, SEEK_SET);
-        fputc('\0', disk_file);
-    }
-
-    FileSystem fs;
-    initialize(&fs);
-
-    // Загрузка файловой системы из файла диска (если существует)
-
-    // Цикл команд
     char command[255];
+
     while (1) {
-        printf("%s>", fs.current_path);
+        char current_path[1024] = "";
+        get_current_path(current_directory, current_path);
+        printf("%s>", current_path);
         scanf("%s", command);
 
-        if (strcmp(command, "format") == 0) {
-            formatDisk(&fs);
-            printf("Disk formatted\n");
+        if (strcmp(command, "exit") == 0) {
+            break;
         } else if (strcmp(command, "ls") == 0) {
-            listDirectory(&fs);
+            list_directory(current_directory);
+        } else if (strcmp(command, "cd") == 0) {
+            char target[255];
+            scanf("%s", target);
+            current_directory = change_directory(current_directory, root, target);
         } else if (strcmp(command, "mkdir") == 0) {
             char name[255];
             scanf("%s", name);
-            createDirectory(&fs, name);
-            printf("Directory '%s' created\n", name);
+            if (find_entry_by_name(current_directory, name) == NULL) {
+                create_directory(current_directory, name);
+            } else {
+                printf("Папка с таким именем уже существует.\n");
+            }
         } else if (strcmp(command, "touch") == 0) {
             char name[255];
+            char content[1024];
             scanf("%s", name);
-            createFile(&fs, name);
-            printf("File '%s' created\n", name);
-        } else if (strcmp(command, "cd") == 0) {
-            char path[255];
-            scanf("%s", path);
-            changeDirectory(&fs, path);
+            scanf("%s", content);
+            if (find_entry_by_name(current_directory, name) == NULL) {
+                create_file(current_directory, name, content);
+            } else {
+                printf("Файл с таким именем уже существует.\n");
+            }
         } else {
-            printf("Unknown command: %s\n", command);
+            printf("Неизвестная команда.\n");
         }
     }
 
-    // Сохранение файловой системы в файл диска перед завершением программы
+    // Очистка памяти
+    free(root);
 
     return 0;
 }
