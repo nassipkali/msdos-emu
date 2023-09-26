eae2e8dfcbd7696e4512fa48d0b6d024d7eea116
uralskdev <uralskdev@gmail.com>
2023-09-26 17:09:37 +0500

    Added file load/save

--- main.c
+++ main.c
@@ -5,70 +5,81 @@
 // Максимальное количество поддиректорий
 #define MAX_SUBDIRECTORIES 100
 
+// Максимальное количество entry
+#define MAX_ENTRIES 10000
+
 // Структура для представления каталогов и файлов
 typedef struct Entry {
     char name[255];
     int is_directory;
     char content[1024];
-    struct Entry *sub_entries[MAX_SUBDIRECTORIES];
+    int sub_entries[MAX_SUBDIRECTORIES];
     int num_sub_entries;
-    struct Entry *parent;
+    int parent; // Index of the parent entry
 } Entry;
 
+// Глобальный массив entry
+Entry entries[MAX_ENTRIES];
+
 // Функция для создания новой записи (файла или папки)
-Entry* create_entry(char name[], int is_directory, char content[], Entry *parent) {
-    Entry *entry = (Entry*)malloc(sizeof(Entry));
-    strcpy(entry->name, name);
-    entry->is_directory = is_directory;
-    strcpy(entry->content, content);
-    entry->num_sub_entries = 0;
-    entry->parent = parent;
-    return entry;
+int create_entry(Entry entries[], char name[], int is_directory, char content[], int parent) {
+    static int entryIndex = 0;
+    if (entryIndex < MAX_SUBDIRECTORIES) {
+        strcpy(entries[entryIndex].name, name);
+        entries[entryIndex].is_directory = is_directory;
+        strcpy(entries[entryIndex].content, content);
+        entries[entryIndex].num_sub_entries = 0;
+        entries[entryIndex].parent = parent;
+        return entryIndex++;
+    }
+    return -1; // Return -1 to indicate failure (array is full)
 }
 
 // Функция для добавления подзаписи (файла или папки) к записи
-void add_sub_entry(Entry *parent, Entry *sub_entry) {
-    if (parent->num_sub_entries < MAX_SUBDIRECTORIES) {
-        parent->sub_entries[parent->num_sub_entries] = sub_entry;
-        parent->num_sub_entries++;
+void add_sub_entry(Entry entries[], int parentIndex, int subEntryIndex) {
+    if (entries[parentIndex].num_sub_entries < MAX_SUBDIRECTORIES) {
+        entries[parentIndex].sub_entries[entries[parentIndex].num_sub_entries] = subEntryIndex;
+        entries[parentIndex].num_sub_entries++;
     }
 }
 
 // Функция для поиска записи (файла или папки) по имени внутри папки
-Entry* find_entry_by_name(Entry *parent, const char *name) {
-    for (int i = 0; i < parent->num_sub_entries; i++) {
-        if (strcmp(parent->sub_entries[i]->name, name) == 0) {
-            return parent->sub_entries[i];
+int find_entry_by_name(Entry entries[], int parentIndex, const char *name) {
+    for (int i = 0; i < entries[parentIndex].num_sub_entries; i++) {
+        int subEntryIndex = entries[parentIndex].sub_entries[i];
+        if (strcmp(entries[subEntryIndex].name, name) == 0) {
+            return subEntryIndex;
         }
     }
-    return NULL;
+    return -1; // Return -1 to indicate not found
 }
 
 // Функция для вывода содержимого текущей директории
-void list_directory(Entry *current_directory) {
-    if (current_directory->parent != NULL) {
+void list_directory(Entry entries[], int currentDirectoryIndex) {
+    if (currentDirectoryIndex != 0) {
         printf(".\n");
         printf("..\n");
     }
     else {
         printf(".\n");
     }
-    for (int i = 0; i < current_directory->num_sub_entries; i++) {
-        printf("%s\n", current_directory->sub_entries[i]->name);
+    for (int i = 0; i < entries[currentDirectoryIndex].num_sub_entries; i++) {
+        int subEntryIndex = entries[currentDirectoryIndex].sub_entries[i];
+        printf("%s\n", entries[subEntryIndex].name);
     }
 }
 
 // Function to change the current directory
-Entry* change_directory(Entry *current_directory, Entry *root, const char *target) {
-    if (strcmp(target, "..") == 0 && current_directory->parent != NULL) {
+int change_directory(Entry entries[], int currentDirectoryIndex, const char *target) {
+    if (strcmp(target, "..") == 0 && currentDirectoryIndex != 0) {
         // Move up one level
-        return current_directory->parent;
+        return entries[currentDirectoryIndex].parent;
     } else if (strcmp(target, ".") == 0 || strcmp(target, "") == 0) {
         // Stay in the current directory
-        return current_directory;
+        return currentDirectoryIndex;
     } else if (strcmp(target, "/") == 0) {
         // Change to the root directory
-        return root;
+        return 0;
     }
 
     if (target[0] == '.' && target[1] == '/') {
@@ -76,140 +87,186 @@ Entry* change_directory(Entry *current_directory, Entry *root, const char *targe
         target += 2; // Skip the "./" prefix
     } else if (target[0] == '/') {
         // Handle absolute paths
-        current_directory = root;
+        currentDirectoryIndex = 0;
         target++; // Skip the leading '/'
     }
 
-    char *path_copy = strdup(target);
-    char *token = strtok(path_copy, "/");
-    Entry *new_directory = current_directory;
+    char *pathCopy = strdup(target);
+    char *token = strtok(pathCopy, "/");
+    int newDirectoryIndex = currentDirectoryIndex;
 
     while (token != NULL) {
         if (strcmp(token, "..") == 0) {
-            if (new_directory->parent != NULL) {
-                new_directory = new_directory->parent;
+            if (entries[newDirectoryIndex].parent != -1) {
+                newDirectoryIndex = entries[newDirectoryIndex].parent;
             }
         } else {
-            Entry *found_entry = find_entry_by_name(new_directory, token);
+            int foundEntryIndex = find_entry_by_name(entries, newDirectoryIndex, token);
 
-            if (found_entry != NULL && found_entry->is_directory) {
-                new_directory = found_entry;
+            if (foundEntryIndex != -1 && entries[foundEntryIndex].is_directory) {
+                newDirectoryIndex = foundEntryIndex;
             } else {
                 printf("Folder not found: %s\n", token);
-                free(path_copy);
-                return current_directory;
+                free(pathCopy);
+                return currentDirectoryIndex;
             }
         }
         token = strtok(NULL, "/");
     }
 
-    free(path_copy);
-    return new_directory;
+    free(pathCopy);
+    return newDirectoryIndex;
 }
 
 // Функция для создания новой папки
-void create_directory(Entry *current_directory, const char *name) {
-    Entry *new_directory = create_entry(name, 1, "", current_directory);
-    add_sub_entry(current_directory, new_directory);
+int create_directory(Entry entries[], int currentDirectoryIndex, const char *name) {
+    int newDirectoryIndex = create_entry(entries, name, 1, "", currentDirectoryIndex);
+    if (newDirectoryIndex != -1) {
+        add_sub_entry(entries, currentDirectoryIndex, newDirectoryIndex);
+    } else {
+        printf("Maximum subdirectories reached.\n");
+    }
+    return newDirectoryIndex;
 }
 
 // Функция для создания нового файла
-void create_file(Entry *current_directory, const char *name, const char *content) {
-    Entry *new_file = create_entry(name, 0, content, current_directory);
-    add_sub_entry(current_directory, new_file);
+void create_file(Entry entries[], int currentDirectoryIndex, const char *name, const char *content) {
+    int newFileIndex = create_entry(entries, name, 0, content, currentDirectoryIndex);
+    if (newFileIndex != -1) {
+        add_sub_entry(entries, currentDirectoryIndex, newFileIndex);
+    } else {
+        printf("Maximum subdirectories reached.\n");
+    }
 }
 
-// Функция для получения полного пути текущей директории
-void get_current_path(Entry *current_directory, char *path) {
-    Entry *stack[MAX_SUBDIRECTORIES]; // Стек для хранения директорий
-    int depth = 0; // Глубина стека
+// Function to get the full path of the current directory
+void get_current_path(Entry entries[], int currentDirectoryIndex, char *path) {
+    int stack[MAX_SUBDIRECTORIES]; // Stack to store directory indices
+    int depth = 0; // Depth of the stack
 
-    // Помещаем директории в стек, начиная с текущей и двигаясь вверх по иерархии
-    while (current_directory != NULL) {
-        stack[depth] = current_directory;
-        current_directory = current_directory->parent;
+    // Traverse up the directory hierarchy, starting from the current directory
+    while (currentDirectoryIndex != -1) {
+        stack[depth] = currentDirectoryIndex;
+        currentDirectoryIndex = entries[currentDirectoryIndex].parent;
         depth++;
     }
 
     if (depth == 1) {
+        // Special case for the root directory
         strcat(path, "/");
         return;
     }
 
-    depth--; // skip root dir
-
-    // Собираем путь, начиная с корневой директории и двигаясь вниз по стеку
-    while (depth > 0) {
-        depth--;
+    // Build the path by descending the stack, skipping the root directory
+    for (int i = depth - 2; i >= 0; i--) {
         strcat(path, "/");
-        strcat(path, stack[depth]->name);
+        strcat(path, entries[stack[i]].name);
     }
 }
 
 // Function to clear the content of an Entry and its sub-entries
-void clear_entry(Entry *entry) {
-    if (entry->is_directory) {
-        for (int i = 0; i < entry->num_sub_entries; i++) {
-            clear_entry(entry->sub_entries[i]);
-            free(entry->sub_entries[i]);
+void clear_entry(Entry entries[], int entryIndex) {
+    if (entries[entryIndex].is_directory) {
+        for (int i = 0; i < entries[entryIndex].num_sub_entries; i++) {
+            clear_entry(entries, entries[entryIndex].sub_entries[i]);
         }
-        entry->num_sub_entries = 0; // Clear the number of sub-entries
+        entries[entryIndex].num_sub_entries = 0; // Clear the number of sub-entries
     } else {
-        entry->content[0] = '\0'; // Clear file content
+        entries[entryIndex].content[0] = '\0'; // Clear file content
     }
 }
 
-int main() {
-    Entry *root = create_entry("", 1, "", NULL);
-    Entry *current_directory = root;
+// Function to save the file system to a file
+void save_to_file(Entry entries[], const char *filename) {
+    FILE *file = fopen(filename, "wb"); // Open the file for writing in binary mode
 
+    if (file == NULL) {
+        printf("Error opening file for writing.\n");
+        return;
+    }
+
+    // Write the entire entries array to the file
+    fwrite(entries, sizeof(Entry), MAX_ENTRIES, file);
+
+    fclose(file);
+}
+
+// Function to load the file system from a file
+void load_from_file(Entry entries[], const char *filename) {
+    FILE *file = fopen(filename, "rb"); // Open the file for reading in binary mode
+
+    if (file == NULL) {
+        printf("Error opening file for reading.\n");
+        return;
+    }
+
+    // Read the entire entries array from the file
+    fread(entries, sizeof(Entry), MAX_ENTRIES, file);
+
+    fclose(file);
+}
+
+int main(int argc, char *argv[]) {
+    int rootIndex, currentDirectoryIndex;
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    const char *filename = argv[1];
+
+    memset(entries, 0, sizeof(entries)); // Initialize entries to zero
+
+    // Check if the file exists and load the file system if it does
+    FILE *checkFile = fopen(filename, "rb");
+    if (checkFile != NULL) {
+        fclose(checkFile);
+        load_from_file(entries, filename);
+        printf("File system loaded from %s.\n", filename);
+    } else {
+        // Create a new root directory if the file does not exist
+        int rootIndex = create_entry(entries, "", 1, "", -1);
+        int currentDirectoryIndex = rootIndex;
+        printf("New file system created.\n");
+    }
     char command[255];
 
     while (1) {
-        char current_path[1024] = "";
-        get_current_path(current_directory, current_path);
-        printf("%s>", current_path);
+        char currentPath[1024] = "";
+        get_current_path(entries, currentDirectoryIndex, currentPath);
+        printf("%s>", currentPath);
         scanf("%s", command);
 
         if (strcmp(command, "exit") == 0) {
             break;
         } else if (strcmp(command, "ls") == 0) {
-            list_directory(current_directory);
+            list_directory(entries, currentDirectoryIndex);
         } else if (strcmp(command, "cd") == 0) {
             char target[255];
             scanf("%s", target);
-            current_directory = change_directory(current_directory, root, target);
+            currentDirectoryIndex = change_directory(entries, currentDirectoryIndex, target);
         } else if (strcmp(command, "mkdir") == 0) {
             char name[255];
             scanf("%s", name);
-            if (find_entry_by_name(current_directory, name) == NULL) {
-                create_directory(current_directory, name);
-            } else {
-                printf("Папка с таким именем уже существует.\n");
+            int newDirectoryIndex = create_directory(entries, currentDirectoryIndex, name);
+            if (newDirectoryIndex == -1) {
+                printf("Folder not created.\n");
             }
         } else if (strcmp(command, "touch") == 0) {
             char name[255];
             char content[1024];
             scanf("%s", name);
             scanf("%s", content);
-            if (find_entry_by_name(current_directory, name) == NULL) {
-                create_file(current_directory, name, content);
-            } else {
-                printf("Файл с таким именем уже существует.\n");
-            }
+            create_file(entries, currentDirectoryIndex, name, content);
         } else if (strcmp(command, "format") == 0) {
-            clear_entry(root);
-            root = create_entry("", 1, "", NULL);
-            current_directory = root;
+            clear_entry(entries, rootIndex);
+            rootIndex = create_entry(entries, "", 1, "", -1);
+            currentDirectoryIndex = rootIndex;
             printf("Ok\n");
-        }
-        else {
-            printf("Неизвестная команда.\n");
+        } else {
+            printf("Unknown command.\n");
         }
     }
-
-    // Очистка памяти
-    free(root);
-
+    save_to_file(entries, filename);
     return 0;
 }
